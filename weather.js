c0276334b954406092d97378d8538cfc82abd462
Eugene Kapelko <evgeniy.kapelko.c@balanceinternet.com.au>
2022-12-06 19:14:25 +0200

    Implementation the save token in file

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', args.t);
+        printSuccess('Token has beeen saved');
+    } catch (error) {
+        printError('Something went wrong. Please try again.');
+        printError(error.message);
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
@@ -9,6 +20,13 @@ const initCLI = () => {
 
     if (args.h) {
         printHelp();
+    }
+    
+    if (args.s) {
+    }
+
+    if (args.t) {
+       return saveToken(args.t);
     }
 }
 
